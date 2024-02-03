68a0d3d35c9b966fc488ccc81279299ec8a93bf9
McFly1208 <car.barb.0904@gmail.com>
2024-02-02 19:32:43 -0600

    end of te section 5

--- src/app/pokemons/index.ts
+++ src/app/pokemons/index.ts
@@ -1,3 +1,4 @@
 export type {PokemonResponse} from './interfaces/pokemon-response';
 export type {SimplePokemon} from './interfaces/simple-pokemon';
 export {PokemonGrid} from './components/PokemonGrid';
+export type {Pokemon} from './interfaces/pokemon';
